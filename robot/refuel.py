f027810c98262c05807d487c904cd073af13dd1e
sagelin <charlie.linacre24@googlemail.com>
2015-08-18 15:09:38 +0100

    Running robot/refuel.py

--- robot/refuel.py
+++ robot/refuel.py
@@ -18,12 +18,10 @@ while score < 32:
       if touch() == 'fruit':
        move()
        score += 1
-       break
   if look() =='wall':
     print "wall"
     turn(1)
     turncounter += 1
   if turncounter == 4:
-    print "counter"
     turncounter = 0
     move()
