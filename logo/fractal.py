8390f513ec7fc9c713fe04a5baf9e20d11fb897f
sagelin <charlie.linacre24@googlemail.com>
2015-08-18 12:27:59 +0100

    Running logo/fractal.py

--- logo/fractal.py
+++ logo/fractal.py
@@ -1,14 +1,14 @@
 from tealight.logo import move, turn
 
 # Draws the von Koch Snowflake curve
-
+angle = 60
 def fractal(scale, detail):
   move(scale)
   for i in range(0, int(detail)):
-    turn(60)
+    turn(angle)
     move(scale/2)
     detail =- 1
+   
     
    
-
 fractal(100,3)
