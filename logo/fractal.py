737bb3c9bab04d041fcc37e8866bcd72cd82ba02
sagelin <charlie.linacre24@googlemail.com>
2015-08-18 12:13:10 +0100

    Closing logo/fractal.py

--- logo/fractal.py
+++ logo/fractal.py
@@ -2,24 +2,4 @@ from tealight.logo import move, turn
 
 # Draws the von Koch Snowflake curve
 
-def segment(scale, detail):
-    move(scale)
-    turn(120)
-    if detail > 0:
-      segment(scale/2, detail -1)
-    move(scale)
-    turn(120)
-    if detail > 0:
-      segment(scale/2, detail -1)
-    move(scale)
-    turn(120)
-    if detail > 0:
-      segment(scale/2, detail -1)
-
-def tree(scale, detail):
-  
-    
-    
-      
-turn(90)
-segment(300,4)
+def 
