88477f8fbe4071431ae082e06eeef7e9dbac32ed
sagelin <charlie.linacre24@googlemail.com>
2015-08-18 15:19:10 +0100

    Running robot/refuel.py

--- robot/refuel.py
+++ robot/refuel.py
@@ -7,17 +7,17 @@ from tealight.robot import (move,
                        right_side)
 
 turncounter = 0
-
-print "breaking"
-if look() =='wall':
-  print "wall"
-  turn(1)
-  turncounter += 1
-if turncounter == 4:
-  turncounter = 0
-  move()
-if look() =='fruit':
-  turncounter = 0
+while True:
+  print "breaking"
+  if look() =='wall':
+    print "wall"
+    turn(1)
+    turncounter += 1
+  if turncounter == 4:
+    turncounter = 0
+    move()
+  if look() =='fruit':
+    turncounter = 0
   while look() =="fruit":
     if touch() == 'None':
       move()
