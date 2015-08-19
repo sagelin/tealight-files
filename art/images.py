9c032237f5ae4f301779b466d1c35cabac347c45
sagelin <charlie.linacre24@googlemail.com>
2015-08-19 09:32:41 +0100

    Running art/images.py

--- art/images.py
+++ art/images.py
@@ -8,8 +8,8 @@ height = 8
 
 for i in range(0,width):
   if i % 4 == 0:
-  for j in range(0,height):
-    if i % 4 == 0:
-      image(x + i * 60, y + j * 60, "misc/YellowFlower.png")
-    else:
-      image(x + i * 60, y + j * 60, "misc/Clover.png")
+    for j in range(0,height):
+      if i % 4 == 0:
+        image(x + i * 60, y + j * 60, "misc/YellowFlower.png")
+      else:
+        image(x + i * 60, y + j * 60, "misc/Clover.png")
