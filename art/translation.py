a95afb94b16d8f5cd8446aadbeb41703e3de0af6
sagelin <charlie.linacre24@googlemail.com>
2015-08-21 10:41:49 +0100

    Closing art/translation.py

--- art/translation.py
+++ art/translation.py
@@ -6,3 +6,17 @@ def translation(points, x,y):
   for each in points:
     each[0]+x
     each[1]+y
+
+def drawcar(points,colour):
+  color(colour)
+  temppoints = []
+  counter = 0 
+  for each in points:
+    temppoints.append(each[0])
+    temppoints.append(each[1])
+    counter += 1
+    if counter == 2:
+      line(temppoints[0],temppoints[1],temppoints[2],temppoints[3])
+      temppoints =[]
+      counter = 0
+
