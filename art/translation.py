6c87f888f9ab154ba4afd50e16e108d5821c5ba8
sagelin <charlie.linacre24@googlemail.com>
2015-08-21 10:40:48 +0100

    Closing art/translation.py

--- art/translation.py
+++ art/translation.py
@@ -2,4 +2,7 @@ car = [(x,y),(x+50,y),(x-40,y-25),(x-40,y+25),(x-40,y-25)
        ,(x,y),(x-40,y+25),(x,y),(x-40,y+25),(x+50,y),
        (x-40,y-25),(x+50,y)]
 
-
+def translation(points, x,y):
+  for each in points:
+    each[0]+x
+    each[1]+y
