8cb7b3b60a25b586d967b548e1a9d1df7007abfa
sagelin <charlie.linacre24@googlemail.com>
2015-08-21 10:32:55 +0100

    Running art/rotation.py

--- art/rotation.py
+++ art/rotation.py
@@ -1,14 +1,9 @@
 from tealight.art import *
 from math import * 
+carcolour = "red"
 y = screen_height/4
 x = screen_width/4
 
-
-keys = {"left": False,
-        "right": False,
-        "up": False,
-        "down": False,
-        }
 
 car = [(x,y),(x+50,y),(x-40,y-25),(x-40,y+25),(x-40,y-25)
        ,(x,y),(x-40,y+25),(x,y),(x-40,y+25),(x+50,y),
@@ -24,9 +19,8 @@ def rotation(shape, theta):
    newcar = newshape
    print(newcar)
    return newcar
-
-
-def drawcar(points,colour,x,y):
+      
+def drawcar(points,colour):
   color(colour)
   temppoints = []
   counter = 0 
@@ -35,10 +29,28 @@ def drawcar(points,colour,x,y):
     temppoints.append(each[1])
     counter += 1
     if counter == 2:
-      line(x+temppoints[0],y+temppoints[1],x+temppoints[2],y+temppoints[3])
+      line(temppoints[0],temppoints[1],temppoints[2],temppoints[3])
       temppoints =[]
       counter = 0
-      
+#moved = False 
+
+        
+
+  #if moved == True:
+    
+   # drawcar(newcar,"white")
+  #elif moved == False:
+  #  drawcar(newcar,"blue")
+  
+keys = {"left": False,
+        "right": False,
+        "up": False,
+        "down": False,
+        }
+   
+acceleration = 0
+forwardv = 10 
+
 def handle_keydown(key): 
   keys[key] = True
   return
@@ -49,14 +61,18 @@ def handle_keyup(key):
   return 
     
 def handle_frame():
-  global forwardv, decelerate,theta, x, y, vx, vy, keys
-    if keys["right"] == True:
-      theta += 2
-    elif keys["left"] == True:
-      theta -= 2
-    newcar = rotation(car, theta)
-    color("white")
-    box(0,0,screen_width,screen_height)
-    drawcar(newcar,"blue", x, y)
-  
-
+  global forwardv, decelerate,theta, x, y, vx, vy, keys 
+  if keys["up"] == True:
+    forwardv += 0.1
+  elif keys ["up"] == False:
+    forwardv -= 0.1
+  elif keys["down"] == True:
+    forwardv -= acceleration
+  if keys ["right"] == True:
+    theta += 2
+  elif keys["left"] == True:
+    theta -= 2
+  newcar = rotation(car, theta)
+  color("white")
+  box(0,0,screen_width,screen_height)
+  drawcar(newcar,"blue",)
