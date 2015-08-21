b316400aa0667dc2e556050b796e35341abea853
sagelin <charlie.linacre24@googlemail.com>
2015-08-21 10:19:04 +0100

    Closing art/rotation.py

--- art/rotation.py
+++ art/rotation.py
@@ -1,6 +1,12 @@
 from tealight.art import *
 from math import * 
 
+keys = {"left": False,
+        "right": False,
+        "up": False,
+        "down": False,
+        }
+
 car = [(x,y),(x+50,y),(x-40,y-25),(x-40,y+25),(x-40,y-25)
        ,(x,y),(x-40,y+25),(x,y),(x-40,y+25),(x+50,y),
        (x-40,y-25),(x+50,y)]
