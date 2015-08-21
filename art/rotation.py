9509efdc436039724ef77f55d7deacb021c3ef4f
sagelin <charlie.linacre24@googlemail.com>
2015-08-21 10:17:14 +0100

    Closing art/rotation.py

--- art/rotation.py
+++ art/rotation.py
@@ -1,2 +1,17 @@
 from tealight.art import *
 from math import * 
+
+car = [(x,y),(x+50,y),(x-40,y-25),(x-40,y+25),(x-40,y-25)
+       ,(x,y),(x-40,y+25),(x,y),(x-40,y+25),(x+50,y),
+       (x-40,y-25),(x+50,y)]
+    
+theta = 0
+def rotation(shape, theta):
+   theta = radians(theta)
+   newshape = []
+   for vertex in shape:
+      print(vertex[0],vertex[1])
+      newshape.append((((vertex[0]-x)*cos(theta)-(vertex[1]-y)*sin(theta))+x,((vertex[0]-x)*sin(theta)+(vertex[1]-y)*cos(theta)+y)))
+   newcar = newshape
+   print(newcar)
+   return newcar
